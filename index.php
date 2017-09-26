ceb960838d143bbcbca7ef006678e4c559b0d4fd
fares Bakh <fbakhshwain@github.com>
2017-09-25 23:55:29 -0400

    final draft of php

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
-<h1> man </h1>
+<h1> welcome to the website </h1>
+	<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Login</legend>
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+ 
+<label for='username' >UserName*:</label>
+<input type='text' name='username' id='username'  maxlength="50" />
+ 
+<label for='password' >Password*:</label>
+<input type='password' name='password' id='password' maxlength="50" />
+ 
+<input type='submit' name='Submit' value='Submit' />
+ 
+</fieldset>
+</form>
+
 <?php
-$logins = array(
-    'username1' => 'password1',
-    'username2' => 'password2',
-    'username3' => 'password3',
-);
+//assigns the username and password
+$username = array("admin", "frank", "money");
+$password = array("password", "thepass", "test");
 
-$user = isset($_POST['user']) ? strtolower($_POST['user']) : '';
-$pass = isset($_POST['pass']) ? $_POST['pass'] : '';
+//reads the users input and assigns a name
+$user = $_POST['username'];
+$pass = $_POST['password'];
 
-if ( ! isset($logins[$user]) or $logins[$user] != $pass)
-{
-    showForm("Wrong Username/Password");
-    exit();
+
+//checks to see if the username and password are correct
+if($user == $username[count($username)] && $pass == $password[count($password)]) {
+    echo "welcome";
 }
+else {
+     echo "wrong input, try again ";
+}
+
 ?>
-</body>
-</html>
